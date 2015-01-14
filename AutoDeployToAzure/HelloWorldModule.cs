ab753ff1977c553544cdf1ae7f7e5365e384e45a
Stefan Moser <stefanm@iqmetrix.com>
2015-01-14 13:19:43 -0600

    hello world

---
+++ AutoDeployToAzure/HelloWorldModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+
+namespace AutoDeployToAzure
+{
+    public class HelloWorldModule : NancyModule
+    {
+        public HelloWorldModule() : base("/")
+        {
+            Get["/"] = HelloWorld;
+        }
+
+        private object HelloWorld(dynamic parameters)
+        {
+            return "Hello world!";
+        }
+    }
+}
